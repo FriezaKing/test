e603408d4b21d523a38967cb6c1fd66d8b1d0447
孙晓 <xiao.sun@esmartwave.com>
2021-12-31 14:46:55 +0800

    添加日志

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace WebApplication1.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("hello baby.");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
